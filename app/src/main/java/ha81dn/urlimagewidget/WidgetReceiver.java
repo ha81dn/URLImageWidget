c0113d015d864a18e959051f6496e298eade1a1e
ha81dn <holger.arndt@gmail.com>
2017-09-10 14:20:47 +0200

    ignore cert

--- app/src/main/java/ha81dn/urlimagewidget/WidgetReceiver.java
+++ app/src/main/java/ha81dn/urlimagewidget/WidgetReceiver.java
@@ -1,5 +1,6 @@
 package ha81dn.urlimagewidget;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -11,20 +12,28 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
 
 
 public class WidgetReceiver extends AppWidgetProvider {
-    static AsyncTask<String, Void, Bitmap> currentTask;
+    static AsyncTask<String, String, Bitmap> currentTask;
 
     private static void prepareWidget(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int widgetId : appWidgetIds) {
@@ -92,16 +101,48 @@ public class WidgetReceiver extends AppWidgetProvider {
         }
     }
 
-    private static class HttpAsyncTask extends AsyncTask<String, Void, Bitmap> {
+    private static class NullHostNameVerifier implements HostnameVerifier {
+        @SuppressLint("BadHostnameVerifier")
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
+
+    private static class NullX509TrustManager implements X509TrustManager {
+        @SuppressLint("TrustAllX509TrustManager")
+        @Override
+        public void checkClientTrusted(final X509Certificate[] chain,
+                                       final String authType) throws CertificateException {
+        }
+
+        @SuppressLint("TrustAllX509TrustManager")
+        @Override
+        public void checkServerTrusted(final X509Certificate[] chain,
+                                       final String authType) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
+    private static class HttpAsyncTask extends AsyncTask<String, String, Bitmap> {
         AppWidgetManager appWidgetManager;
         Context context;
         int targetWidgetId;
-        long lastTime;
+        //long lastTime;
 
         @Override
         protected Bitmap doInBackground(String... urls) {
             try {
                 if (urls[0].equals("")) return null;
+                HttpsURLConnection.setDefaultHostnameVerifier(new NullHostNameVerifier());
+                SSLContext context = SSLContext.getInstance("TLS");
+                context.init(null, new X509TrustManager[]{new NullX509TrustManager()}, new SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+
                 URL myFileUrl = new URL(urls[0]);
                 HttpsURLConnection conn = (HttpsURLConnection) myFileUrl.openConnection();
                 conn.setDoInput(true);
@@ -109,13 +150,17 @@ public class WidgetReceiver extends AppWidgetProvider {
                 InputStream is = conn.getInputStream();
                 return BitmapFactory.decodeStream(is);
             } catch (Exception error) {
-                Toast.makeText(context, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                publishProgress(error.getLocalizedMessage());
             }
             return null;
         }
 
         @Override
-        protected void onProgressUpdate(Void... voids) {
+        protected void onProgressUpdate(String... msg) {
+            try {
+                Toast.makeText(context, msg[0], Toast.LENGTH_LONG).show();
+            } catch (Exception ignore) {
+            }
             /*
             try {
                 long now = System.currentTimeMillis();
@@ -150,7 +195,12 @@ public class WidgetReceiver extends AppWidgetProvider {
             sdf = new SimpleDateFormat("dd.MM.yyyy, HH:mm 'Uhr'", Locale.getDefault());
             now = Calendar.getInstance();
             remoteViews.setTextViewText(R.id.timestamp, sdf.format(now.getTime()));
-            remoteViews.setTextViewText(R.id.location, sharedPref.getString("name_" + targetWidgetId, ""));
+            String tmp = sharedPref.getString("name_" + targetWidgetId, "");
+            if (tmp.equals("")) {
+                remoteViews.setViewVisibility(R.id.location, View.GONE);
+            } else {
+                remoteViews.setTextViewText(R.id.location, tmp);
+            }
 
             ComponentName thisWidget = new ComponentName(context, WidgetReceiver.class);
             int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
